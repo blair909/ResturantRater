543467ce73b1bf882c7082c8cb62876964bf04c8
blair909 <61510475+blair909@users.noreply.github.com>
2020-05-11 15:34:42 -0500

    Set up DbContext

---
+++ ResturantRater/Models/Resturant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+
+namespace ResturantRater.Models
+{
+    public class Resturant
+    {
+        public int ResturantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class ResturantDbContext : DbContext
+    {
+        public DbSet<Resturant> Resturants { get; set; }
+}
