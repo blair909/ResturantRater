78a93e449f5a658c255fdae9951d8491dbb06940
blair909 <61510475+blair909@users.noreply.github.com>
2020-05-11 16:34:17 -0500

    Setup Resturant Index View

--- ResturantRater/Controllers/ResturantController.cs
+++ ResturantRater/Controllers/ResturantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResturantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace ResturantRater.Controllers
 {
     public class ResturantController : Controller
     {
+        private ResturantDbContext _db = new ResturantDbContext();
+
         // GET: Resturant/Index
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Resturants.ToList());
         }
     }
 }
